0d66a6c5c9ea3d80af78f35a44375f31e1beeda8
Luke Foust <luke@foust.com>
2015-12-15 20:52:17 -0800

    Add RSA Key Generator

---
+++ src/RsaKeyGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RsaKeyGenerator
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (RSACryptoServiceProvider rs = new RSACryptoServiceProvider(2048))
+            {
+                string xml = rs.ToXmlString(includePrivateParameters: true);
+                Console.WriteLine(xml);
+                Console.ReadLine();
+            }
+        }
+    }
+}
